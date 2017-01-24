e78b3c620381fc02c7b83dc413bff1cc56586e49
Maciej Sz <maciej.sz@gmail.com>
2017-01-24 19:29:05 +0100

    added config-file-update.sh script

---
+++ lib/config-file-update.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+function configFileUpdate() {
+    VAR=$1
+    VAL=$2
+    FILE=$3
+
+    CONTAINS=$(grep -o "^ *${VAR}\=" "${FILE}")
+    if [[ "" == "${CONTAINS}" ]]; then
+        echo -e "\n${VAR}=\"${VAL}\"" >> "${FILE}"
+    else
+        sed -i -e "s/^\( *${VAR}=\)\(.*\)/\1\"${VAL}\"/" "${FILE}"
+    fi
+}
